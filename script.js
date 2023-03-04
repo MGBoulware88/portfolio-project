b046157a3895939a92c9394b75bae11edcb91d8e
tmchuynh <tmchuynh@gmail.com>
2023-03-04 13:01:04 -0800

    image carousel

--- script.js
+++ script.js
@@ -309,3 +309,16 @@ counters.forEach(counter => {
 
 	updateCount();
 });
+
+function rotate() {
+  var lastChild = $('.slider div:last-child').clone();
+  /*$('#test').html(lastChild)*/
+  $('.slider div').removeClass('firstSlide')
+  $('.slider div:last-child').remove();
+  $('.slider').prepend(lastChild)
+  $(lastChild).addClass('firstSlide')
+}
+
+window.setInterval(function(){
+  rotate()
+}, 4000);
