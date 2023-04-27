056e67b8d22af52f02a6fd468101277bea8744f1
Gray-Desktop <gray.boulware@gmail.com>
2023-04-26 21:20:56 -0500

    🎨 style(index.html): update portfolio content The changes made to the index.html file include updating the skills section to reflect the author's current skills, updating the welcome message and personal mission statement to reflect the author's transition into web development, and removing the daily fun fact or joke section. These changes improve the overall content of the portfolio and provide a more accurate representation of the author's skills and goals.
    🔥 chore(script.js): remove unused code and comments
    The code that was commented out and the API code that was no longer being used has been removed. This improves the readability of the code and reduces the file size.
    
    🔥 chore(script_old.js): remove old script file
    The old script file is no longer needed and has been removed.

--- script.js
+++ script.js
@@ -1,78 +1,77 @@
-(function() {
+(function () {
     getProjects();
 })()
-
-const textContainer = document.querySelector("#container");
+/* 
+commenting out all the API code I am no longer using
+*/
+// const textContainer = document.querySelector("#container");
 const project_container = document.querySelector(".project-container");
 
-const userOptions = {
-    "select": () => "Please select Joke or Fact first.",
-    "joke": getJoke,
-    "fact": getFact,
-    "repo": getProjects
-}
+// const userOptions = {
+//     "select": () => "Please select Joke or Fact first.",
+//     "joke": getJoke,
+//     "fact": getFact,
+//     "repo": getProjects
+// }
 
-async function getKey() {
-    var key = document.querySelector("#selection").value;
-    addHTML(key);
-}
+// async function getKey() {
+//     var key = document.querySelector("#selection").value;
+//     addHTML(key);
+// }
 
-async function addHTML(key) {
-    document.querySelector("#daily").reset();
-    while (textContainer.firstChild) {
-        textContainer.removeChild(textContainer.lastChild);
-    }
-    if (userOptions.hasOwnProperty(key)) {
-        createHTML(await userOptions[key]());
-    }  
-}
+// async function addHTML(key) {
+//     document.querySelector("#daily").reset();
+//     while (textContainer.firstChild) {
+//         textContainer.removeChild(textContainer.lastChild);
+//     }
+//     if (userOptions.hasOwnProperty(key)) {
+//         createHTML(await userOptions[key]());
+//     }
+// }
 
-function createHTML(text) {
-    var newHTML = document.createElement("p");
-    textContainer.append(newHTML);
-    newHTML.innerHTML = text;
-}
+// function createHTML(text) {
+//     var newHTML = document.createElement("p");
+//     textContainer.append(newHTML);
+//     newHTML.innerHTML = text;
+// }
 
-async function getJoke() {
-    var response = await axios.get('https://jokeapi-v2.p.rapidapi.com/joke/Any',{
-        params: {
-            type: 'single',
-            format: 'json',
-            idRange: '0-319',
-            blacklistFlags: 'nsfw,racist,religious,political,sexist,explicit'
-          },
-          headers: {
-            'X-RapidAPI-Key': 'af62c2fa06mshf0b765ef4edb5a1p19978ejsn40af1ecfa88c',
-            'X-RapidAPI-Host': 'jokeapi-v2.p.rapidapi.com'
-          }
-    });
-    var jokeText = response.data.joke;
-    return jokeText;
-}
+// async function getJoke() {
+//     var response = await axios.get('https://jokeapi-v2.p.rapidapi.com/joke/Any', {
+//         params: {
+//             type: 'single',
+//             format: 'json',
+//             idRange: '0-319',
+//             blacklistFlags: 'nsfw,racist,religious,political,sexist,explicit'
+//         },
+//         headers: {
+//             'X-RapidAPI-Key': 'af62c2fa06mshf0b765ef4edb5a1p19978ejsn40af1ecfa88c',
+//             'X-RapidAPI-Host': 'jokeapi-v2.p.rapidapi.com'
+//         }
+//     });
+//     var jokeText = response.data.joke;
+//     return jokeText;
+// }
 
-async function getFact() {
-    var response = await axios.get('https://api.api-ninjas.com/v1/facts?limit=1',{
-        headers: {
-            'X-Api-Key': 'NMGNlnLpB+44dYy8e0zpaA==jg3p2eZM2e3kygbW'
-        }
-    });
-    var factText = response.data[0].fact;
-    return factText;
-}
+// async function getFact() {
+//     var response = await axios.get('https://api.api-ninjas.com/v1/facts?limit=1', {
+//         headers: {
+//             'X-Api-Key': 'NMGNlnLpB+44dYy8e0zpaA==jg3p2eZM2e3kygbW'
+//         }
+//     });
+//     var factText = response.data[0].fact;
+//     return factText;
+// }
 
 async function getProjects() {
     $.getJSON("https://api.github.com/users/mgboulware88/repos?per_page=53", function (data) {
-    data.forEach(function (repo) {
-        // $.getJSON(repo.languages_url, function (languages) {
-        //     populate(repo.name, repo.pushed_at, repo, Object.keys(languages));
-        // })
-        if (repo.topics.length > 0) {
-            $.getJSON(repo.languages_url, function (languages) {
-                populate(repo.name, repo.pushed_at, repo, Object.keys(languages));
-            })
-        }
+        data.forEach(function (repo) {
+            if (repo.topics.length > 0) {
+                $.getJSON(repo.languages_url, function (languages) {
+                    populate(repo.name, repo.pushed_at, repo, Object.keys(languages));
+                })
+            }
+        });
     });
-});
 }
 
 function populate(name, pushed_at, repo, languages) {
@@ -121,7 +120,7 @@ function addLanguages(languages, card) {
     if (languages.length == 0) {
         icon.innerHTML = "";
     }
-  
+
     if (languages.includes("HTML")) {
         var icon2 = document.createElement("i");
         icon2.classList.add("bx");
@@ -182,17 +181,17 @@ const hiWords = selectAll('.hi__word');
 const base = select('.hi__base-plate');
 let winW = 0;
 let winH = 0;
-let pointer = { 
-	x: window.innerWidth / 2, 
-	y: window.innerHeight / 2 
+let pointer = {
+    x: window.innerWidth / 2,
+    y: window.innerHeight / 2
 };
 
 function init() {
-    
+
     setWinDimensions();
 
     gsap.set(container, { autoAlpha: 1 });
-    
+
     gsap.timeline({ delay: 0.5 })
         .from('.hi__location--lat', {
             x: 100,
@@ -212,24 +211,24 @@ function init() {
             stagger: 0.14,
             ease: 'elastic(0.4,0.3)'
         }, 0);
-    
-    gsap.to(cuboid, { 
-        rotateX: -360, 
-        duration: 8, 
-        repeat: -1, 
-        ease: 'none' 
+
+    gsap.to(cuboid, {
+        rotateX: -360,
+        duration: 8,
+        repeat: -1,
+        ease: 'none'
     });
-    
-    gsap.fromTo(cuboid, { 
-        rotateY: 8, 
-        rotate: -10 
-    },{ 
-        rotateY: -8, 
+
+    gsap.fromTo(cuboid, {
+        rotateY: 8,
+        rotate: -10
+    }, {
+        rotateY: -8,
         rotate: 10,
-        duration: 2.2, 
-        yoyo: true, 
-        repeat: -1, 
-        ease: 'sine.inOut' 
+        duration: 2.2,
+        yoyo: true,
+        repeat: -1,
+        ease: 'sine.inOut'
     });
 }
 
@@ -239,46 +238,46 @@ function setWinDimensions() {
 }
 
 function calcOffset(xPos, yPos) {
-	let dX = 2*(xPos - winW/2)/winW;
-	let dY = -2*(yPos - winH/2)/winH;
-	return [dX,dY];
+    let dX = 2 * (xPos - winW / 2) / winW;
+    let dY = -2 * (yPos - winH / 2) / winH;
+    return [dX, dY];
 }
 
 function followPointer(pX, pY) {
     let nPos = calcOffset(pX, pY); // get cursor position from center
     let nX = nPos[0];
-	let nY = nPos[1];
-    let positiveX = Math.sqrt(nX*nX);
-	let positiveY = Math.sqrt(nY*nY);
-    let deltaS = 450*positiveX;
-    let deltaW = 600*positiveY;
-	gsap.to(hiWords, {
-		fontStretch: `${(550-deltaS)}%`,
-        fontWeight: 800-deltaW,
-		duration: 2
-	});
+    let nY = nPos[1];
+    let positiveX = Math.sqrt(nX * nX);
+    let positiveY = Math.sqrt(nY * nY);
+    let deltaS = 450 * positiveX;
+    let deltaW = 600 * positiveY;
+    gsap.to(hiWords, {
+        fontStretch: `${(550 - deltaS)}%`,
+        fontWeight: 800 - deltaW,
+        duration: 2
+    });
 }
 
-window.addEventListener("mousemove", function(event) {
-	pointer.x = event.clientX;
-	pointer.y = event.clientY;
-	followPointer(pointer.x, pointer.y);
+window.addEventListener("mousemove", function (event) {
+    pointer.x = event.clientX;
+    pointer.y = event.clientY;
+    followPointer(pointer.x, pointer.y);
 });
 
-window.addEventListener('touchmove', function(event) {
- 	pointer.x = event.touches[0].clientX;
-  	pointer.y = event.touches[0].clientY;
-	followPointer(pointer.x, pointer.y);
+window.addEventListener('touchmove', function (event) {
+    pointer.x = event.touches[0].clientX;
+    pointer.y = event.touches[0].clientY;
+    followPointer(pointer.x, pointer.y);
 });
 
-window.addEventListener('touchstart', function(event) {
- 	pointer.x = event.touches[0].clientX;
-  	pointer.y = event.touches[0].clientY;
-	followPointer(pointer.x, pointer.y);
+window.addEventListener('touchstart', function (event) {
+    pointer.x = event.touches[0].clientX;
+    pointer.y = event.touches[0].clientY;
+    followPointer(pointer.x, pointer.y);
 });
 
 window.onload = () => {
-	init();
+    init();
 };
 
 window.onresize = setWinDimensions;
